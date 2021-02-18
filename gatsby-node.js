ba9c01275934eb878db3e4744ee773f3c85451e7
garlicgarrison <60302232+garlicgarrison@users.noreply.github.com>
2021-02-17 16:26:33 -0800

    fixing pipeline error

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,31 +1,32 @@
 exports.createPages = async ({ actions, graphql, reporter }) => {
-    const { createPage } = actions
-    const SprintTemplate = require.resolve(`./src/components/templates/SprintTemplate.tsx`)
-    const result = await graphql(`
+  const { createPage } = actions
+  const SprintTemplate = require.resolve(`./src/components/templates/SprintTemplate.tsx`)
+  const result = await graphql(`
     {
-        allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC}) {
-          edges {
-            node {
-              frontmatter {
-                date
-                day
-              }
+      allMarkdownRemark(
+        sort: { order: DESC, fields: [frontmatter___date] }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            frontmatter {
+              path
             }
           }
         }
-        
       }
-    `)
-    // Handle errors
-    if (result.errors) {
-      reporter.panicOnBuild(`Error while running GraphQL query.`)
-      return
     }
-    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-      createPage({
-        path: node.frontmatter.path,
-        component: SprintTemplate,
-        context: {},
-      })
-    })
+  `)
+  // Handle errors
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`)
+    return
   }
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.frontmatter.path,
+      component: SprintTemplate,
+      context: {},
+    })
+  })
+}
